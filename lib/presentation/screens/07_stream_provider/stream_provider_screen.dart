43a72b565125aab445c72f301b1a447277fc19df
Moises Silva Cardenas <moises.silva@et.bo>
2024-08-28 11:08:16 -0400

    Stream provider

--- lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
+++ lib/presentation/screens/07_stream_provider/stream_provider_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_app/presentation/providers/stream_provider.dart';
 
 class StreamProviderScreen extends StatelessWidget {
   const StreamProviderScreen({super.key});
@@ -10,6 +11,31 @@ class StreamProviderScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Stream Provider'),
       ),
+      body: const StreamView(),
+    );
+  }
+}
+
+class StreamView extends ConsumerWidget {
+  const StreamView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userinChatAsync = ref.watch(usersInChatProvider);
+    return userinChatAsync.when(
+      loading: () => const Center(child: CircularProgressIndicator()),
+      error: (error, stackTrace) => Center(child: Text("$error")),
+      data: (data) {
+        return ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final user = data[index];
+            return ListTile(
+              title: Text(user),
+            );
+          },
+        );
+      },
     );
   }
 }
